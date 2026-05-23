5c4adcd0048f881f07bf9e99f878681ba8c9725a
brian ramirez <tiziano0705@hotmail.com>
2026-05-23 19:11:56 -0300

    increase express body size limit

--- server/server.js
+++ server/server.js
@@ -43,7 +43,8 @@ app.use(
 );
 
 app.use(cookieParser());
-app.use(express.json());
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 app.use(passport.initialize());
 app.use("/api/auth", authRouter);
 app.use("/api/admin/products", adminProductsRouter);
